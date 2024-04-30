ffa245fe4a1fbcbd831474d65709cd65a083fb73
CelineTrammi <celinetram@gmail.com>
2024-04-30 10:35:45 +0200

    swap numpad and fun layers

--- keyboards/crkbd/keymaps/CelineTrammi/keymap.c
+++ keyboards/crkbd/keymaps/CelineTrammi/keymap.c
@@ -35,18 +35,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------------------------------------------------------------------------------|                    |------------------------------------------------------------------------------------------------|
       XXXXXXX,      KC_Z,         KC_X,           KC_C,           KC_D,           KC_V,                       KC_K,           KC_H,           KC_COMM,            KC_DOT,         KC_SLSH,            XXXXXXX,
   //|--------------------------------------------------------------------------------|                    |------------------------------------------------------------------------------------------------|
-                                                  XXXXXXX,     LT(_NAV, KC_BSPC), LT(_NUM, KC_ENT),           LT(_FN, KC_TAB),LT(_SYM, KC_SPC),  XXXXXXX
+                                                  XXXXXXX,     LT(_NAV, KC_BSPC), LT(_FN, KC_ENT),           LT(_NUM, KC_TAB),LT(_SYM, KC_SPC),  XXXXXXX
   ),
 
     [_SYM] = LAYOUT_split_3x6_3(
   //.--------------------------------------------------------------------.              .-------------------------------------------------------------------.
-      XXXXXXX,      KC_NO,      KC_LPRN,    KC_RPRN,    S(KC_QUOT), KC_QUOT,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
-  //.---------------------------   (   -----    )  -----    "   ----  '  |              |-------------------------------------------------------------------|
+      XXXXXXX,      KC_NO,      KC_LPRN,    KC_RPRN,    KC_QUOT,    KC_NO,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
+  //.---------------------------   (   -----    )  -----    "   ---------|              |-------------------------------------------------------------------|
       XXXXXXX,      KC_BSLS,    KC_LBRC,    KC_RBRC,    KC_EQL,     KC_GRV,             KC_NO,      KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,
   //.---------------  \|   -----   [{  -----   ]}  -----  =+   -----  `~ |              |-------------------------------------------------------------------|
       XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_MINS,    KC_NO,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
   //.---------------------------------------------------   -_  ----------|              |-------------------------------------------------------------------|
-                                            XXXXXXX,    KC_ESC,     KC_BSPC,            _____,      _____,      XXXXXXX
+                                            XXXXXXX,    KC_ESC,     KC_BSPC,            _______,    _______,    XXXXXXX
   ),
     [_NAV] = LAYOUT_split_3x6_3(
   //.-------------------------------------------------------------------.               .-------------------------------------------------------------------.
@@ -56,28 +56,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
       XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_BRID,    XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-                                            XXXXXXX,    _____,      _____,              KC_MPLY,    KC_DEL,     XXXXXXX
+                                            XXXXXXX,    _______,    _______,            KC_MPLY,    KC_DEL,     XXXXXXX
   ),
 
     [_NUM] = LAYOUT_split_3x6_3(
   //.-------------------------------------------------------------------.               .-------------------------------------------------------------------.
-      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_7,       KC_8,       KC_9,       KC_NO,      XXXXXXX,
+      XXXXXXX,      KC_NO,      KC_7,       KC_8,       KC_9,       KC_NO,              KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,              KC_NO,      KC_4,       KC_5,       KC_6,       KC_NO,      XXXXXXX,
+      XXXXXXX,      KC_NO,      KC_4,       KC_5,       KC_6,       KC_NO,              KC_NO,       KC_LSFT,     KC_LCTL,     KC_LALT,     KC_LGUI,    XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_1,       KC_2,       KC_3,       KC_NO,      XXXXXXX,
+      XXXXXXX,      KC_NO,      KC_1,       KC_2,       KC_3,       KC_NO,              KC_NO,       KC_NO,       KC_COMM,     KC_DOT,      KC_NO,      XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-                                            XXXXXXX,    _____,      _____,              KC_BSPC,    KC_0,       XXXXXXX
+                                            XXXXXXX,    KC_0,       KC_ENT,             _______,     _______,     XXXXXXX
   ),
 
     [_FN] = LAYOUT_split_3x6_3(
   //.-------------------------------------------------------------------.               .-------------------------------------------------------------------.
-      XXXXXXX,      KC_F12,     KC_F7,      KC_F8,      KC_F9,      KC_NO,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
+      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_F7,       KC_F8,       KC_F9,       KC_NO,      XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_F11,     KC_F4,      KC_F5,      KC_F6,      KC_NO,              KC_NO,      KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,
+      XXXXXXX,      KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,              KC_NO,      KC_F4,       KC_F5,       KC_F6,       KC_NO,      XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_F10,     KC_F1,      KC_F2,      KC_F3,      KC_NO,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
+      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_F1,       KC_F2,       KC_F3,       KC_NO,      XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-                                            XXXXXXX,    KC_NO,      KC_NO,              ______,     _____,       XXXXXXX
+                                            XXXXXXX,    _______,    _______,            KC_TAB,     KC_NO,       XXXXXXX
   )
 };
